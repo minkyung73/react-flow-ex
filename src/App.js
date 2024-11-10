c8f285e03c67aaf501a502141aa6abebfde1bbbb
minkyung <minkyung7365@gmail.com>
2024-11-11 07:12:41 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import HorizontalFlow from './pages/horizontal-flow';
+import ElkjsTree from './pages/Elkjs-tree';
+import CustomNodeFlow from './pages/custom-nodes';
+import AddNodeOnEdgeDrop from './pages/add-node-on-edge-drop';
+
+
+function renderPage(page) {
+  if(page === 1) return <HorizontalFlow />
+  // else if(page === 2) return <DragAndDrops />
+  else if(page === 3) return <ElkjsTree />
+  else if(page === 4) return <CustomNodeFlow />
+  else if(page === 5) return <AddNodeOnEdgeDrop />
+}
 
 function App() {
+  const [page, setPage] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => setPage(1)}>Horizontal Flow</button>
+        {/* <button onClick={() => setPage(2)}>Drag And Drops</button> */}
+        <button onClick={() => setPage(3)}>Elkjs Tree</button>
+        <button onClick={() => setPage(4)}>Custom Nodes</button>
+        <button onClick={() => setPage(5)}>Add Node On Edge Drops</button>
+      </div>
+      <div>
+        {renderPage(page)}
+      </div>
     </div>
   );
 }
